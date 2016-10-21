4708ca7e0678c0156eec17d008499e8845c1d9c3
Lenora Porter <lporter630@gmail.com>
2016-10-21 18:58:21 -0400

    Understanding the Execution Context, Lexical Environment, and Execution Stack

---
+++ app.js
@@ -0,0 +1,36 @@
+
+function b() {
+    console.log('Called b!');
+}
+
+b();
+
+console.log(a);
+
+var a = 'Hello World!';
+
+console.log(a);
+
+//Seperation
+
+var a = 30;
+console.log(a);
+
+if (a === undefined) {
+    console.log('a is undefined!');
+}
+
+else {
+    console.log('a is defined!');
+}
+
+//Seperation
+
+b();
+console.log(a);
+
+var a = 'Hello World!';
+
+function b() {
+    console.log('Called b!');
+}
